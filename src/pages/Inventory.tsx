54c5b5511473a637aeb3b0f29ae494646021abe3
KenCarbiz <ken@ken.cc>
2026-06-22 22:18:14 -0400

    Wire restored mobile inventory layout

--- src/pages/Inventory.tsx
+++ src/pages/Inventory.tsx
@@ -1,3 +1,15 @@
 import InventoryModern from "./InventoryModern";
+import InventoryMobileRestored from "./InventoryMobileRestored";
 
-export default InventoryModern;
+const Inventory = () => (
+  <>
+    <div className="lg:hidden">
+      <InventoryMobileRestored />
+    </div>
+    <div className="hidden lg:block">
+      <InventoryModern />
+    </div>
+  </>
+);
+
+export default Inventory;
