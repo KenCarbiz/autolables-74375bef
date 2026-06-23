8ce93ed05265cd9aff5d70d2cf450ff1de92d1b5
KenCarbiz <ken@ken.cc>
2026-06-22 22:23:45 -0400

    Refresh inventory routing sync

--- src/pages/Inventory.tsx
+++ src/pages/Inventory.tsx
@@ -1,15 +1,17 @@
 import InventoryModern from "./InventoryModern";
 import InventoryMobileRestored from "./InventoryMobileRestored";
 
-const Inventory = () => (
-  <>
-    <div className="lg:hidden">
-      <InventoryMobileRestored />
-    </div>
-    <div className="hidden lg:block">
-      <InventoryModern />
-    </div>
-  </>
-);
+function Inventory() {
+  return (
+    <>
+      <div className="lg:hidden">
+        <InventoryMobileRestored />
+      </div>
+      <div className="hidden lg:block">
+        <InventoryModern />
+      </div>
+    </>
+  );
+}
 
 export default Inventory;
